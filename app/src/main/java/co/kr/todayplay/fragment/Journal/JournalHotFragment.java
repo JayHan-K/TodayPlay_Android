62ea861ceab81b708db82f6a6e9068a17e592741
HwayeonChoi <hwayeon351@gmail.com>
2020-11-24 14:34:27 +0900

    JournalFragment

---
+++ app/src/main/java/co/kr/todayplay/fragment/Journal/JournalHotFragment.java
@@ -0,0 +1,21 @@
+package co.kr.todayplay.fragment.Journal;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import co.kr.todayplay.R;
+
+public class JournalHotFragment extends Fragment {
+    public JournalHotFragment(){
+
+    }
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_journal, container, false);
+    }
+}
